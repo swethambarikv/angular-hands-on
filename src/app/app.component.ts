237c1422273abab3194302b21b0c278173457889
Sunithra <sunithra.chidhambara@aspiresys.com>
2024-02-28 18:27:51 +0530

    validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Template-Driven Forms';
-  loginUser(item:any)
-  {
-    console.warn(item);
+export class AppComponent implements OnInit {
+  title = 'Template Driven Forms';
+
+  @ViewChild('contactForm') contactForm!: NgForm;
+
+  contact: Contact = {
+    firstname: '',
+    lastname: '',
+    gender: 'male',
+    isToc: true,
+    email: ''
+  };
+
+  ngOnInit() {
+    // Initialization logic if needed
   }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contact);
+      // You can perform further actions such as sending data to a server here
+    }
+  }
+}
+
+interface Contact {
+  firstname: string;
+  lastname: string;
+  gender: string;
+  isToc: boolean;
+  email: string;
 }
